8bab1a7d1f0635c1190c2115be6edb43b5b052ba
Frédéric Haziza <Frederic.Haziza@nbis.se>
2018-04-27 16:36:58 +0200

    A bit of the idea on how to proceed

--- legacryptor/decrypt.py
+++ legacryptor/decrypt.py
@@ -3,6 +3,12 @@
 # import logging
 # LOG = logging.getLogger(__name__)
 
+
+# This module will combine
+# https://github.com/NBISweden/LocalEGA/blob/dev/lega/outgest.py
+# and
+# https://github.com/NBISweden/LocalEGA/blob/dev/lega/openpgp/__main__.py
+
 def decrypt_file(privkey, f):
     raise NotImplementedError('Not yet implemented, but similar to lega/outgest.py')
 
--- legacryptor/reencrypt.py
+++ legacryptor/reencrypt.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 
-def reencrypt_file(privkey, pubkey, f):
-    raise NotImplementedError('Not yet implemented, but similar to lega/outgest.py')
+# This module will stip the header part and only reencrypt it
 
-def reencrypt_files(privkey, pubkey, args):
+# Strip and decrypting the header is done in decrypt.py
+# encrypting the header is done in encrypt.py
+# The AES encrypted bulk of each file is left untouched
+
+def reencrypt_file(privkey, pubkey, f):
+    raise NotImplementedError('Not yet implemented, but similar to decrypt.py + encrypt.py')
+
+def reencrypt_files(privkey, pubkey, args): 
     raise NotImplementedError('Not yet implemented, but similar to lega/outgest.py')
