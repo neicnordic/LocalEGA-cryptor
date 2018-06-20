40ad0090c29fb48449bde371d1a6c96f48404369
Stefan Negru <stefan@blankdots.com>
2018-06-20 15:40:08 +0300

    fix version to str in docs

--- legacryptor/__init__.py
+++ legacryptor/__init__.py
@@ -4,7 +4,7 @@
 cryptographic file format."""
 
 __title__ = 'LocalEGA cryptographic GA4GH utilities'
-__version__ = VERSION = '1'
+__version__ = VERSION = 1
 __author__ = 'Frédéric Haziza <daz@nbis.se>'
 __license__ = 'Apache License 2.0'
 __copyright__ = __title__ + ' @ NBIS Sweden'
